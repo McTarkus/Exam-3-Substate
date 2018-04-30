58c3c37765e6c03efb972ebddd907d52c0848a17
McTarkus <38666790+McTarkus@users.noreply.github.com>
2018-04-30 14:17:12 -0400

    Created New Exam 3

---
+++ app/src/main/java/edu/edu/rosehuman/alumbajt/exam3substate/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.edu.rosehuman.alumbajt.exam3substate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
